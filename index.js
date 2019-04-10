aa6f6e302f979ed5faba399e465452218c6e0328
Myron Mctyer <myronsdev@gmail.com>
2019-04-10 00:43:12 -0500

    Add passport with google strategy. Add basic auth route

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const app = express()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'bud' })
-})
+const app = express()
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log(accessToken)
+    }
+  )
+)
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
