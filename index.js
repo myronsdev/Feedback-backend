479e2996cbf66b425069641f20fb1711c437f07b
Myron Mctyer <myronsdev@gmail.com>
2019-04-10 16:05:44 -0500

    Refactor Google strategy and the auth routes into their own files, respectively.

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+require('./services/passport')
+
 const PORT = process.env.PORT
 
 const app = express()
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken)
-      console.log(refreshToken)
-      console.log(profile)
-    }
-  )
-)
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
