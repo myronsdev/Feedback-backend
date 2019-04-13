029f4c85df1ed6b7c992a2c31c013bac0c272123
Myron Mctyer <myronsdev@gmail.com>
2019-04-13 18:57:23 -0500

    Add basic react routes and add materialize css

---
+++ client/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveryNew = () => <h2>SurveryNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveryNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
