301fa6a48b4d71062a91e4f5f975b1f597cf41eb
Myron Mctyer <myronsdev@gmail.com>
2019-04-15 00:24:33 -0500

    Fetch user and with lifecycle method and add redux action for it

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './Header'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveryNew = () => <h2>SurveryNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveryNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveryNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App
+export default connect(
+  null,
+  actions
+)(App)
