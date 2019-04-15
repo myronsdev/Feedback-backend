18d9d14835ee012e32eb2e767222210cc3b9e6f3
Myron Mctyer <myronsdev@gmail.com>
2019-04-15 02:37:11 -0500

    Add landing route

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>feedbackLoop</h1>
+      Collect all the feedback!
+    </div>
+  )
+}
+
+export default Landing
